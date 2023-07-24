6b124222dd3c54bb64fa704647c0c82fae11b42f
Victor Dornelles <victor.bogdanowdornelles@gmail.com>
2023-07-24 19:06:04 -0300

    Condition to hide sections

--- src/components/Characters/index.jsx
+++ src/components/Characters/index.jsx
@@ -3,7 +3,7 @@ import "./Characters.css";
 
 const Characters = ({name, primaryColor, secondColor, person}) => {
   return (
-    <section className="characters" style={{backgroundColor: secondColor}}>
+    person.length > 0 && <section className="characters" style={{backgroundColor: secondColor}}>
       <h3 style={{borderColor: primaryColor}}>{name}</h3>
       <div className="persons">
         {person.map(ch => <Person name={ch.name} job={ch.job} image={ch.image}></Person>)}
